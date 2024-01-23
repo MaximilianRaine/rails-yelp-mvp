66bfaa321f0a22e5d3027669d3223ab8e2b30818
MAximilianRaine <raine.maximilian@gmail.com>
2024-01-23 21:25:23 +0100

    added db, working on controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+restaurants_data = [
+{ name: "Nobelhart & Schmutzig", address: "Friedrichstraße 218, 10969 Berlin, Germany", phone_number: "030",category: "chinese" },
+{ name: "Bastard Berlin", address: "Reichenberger Str. 122, 10999 Berlin, Germany", phone_number: "030", category: "italian" },
+{ name: "Lokal 6", address: "Gabriel-Max-Straße 6, 10245 Berlin, Germany", phone_number: "030", category: "japanese" },
+{ name: "Coda Dessert Dining", address: "Friedelstraße 47, 12047 Berlin, Germany", phone_number: "030", category: "french" },
+{ name: "Prater Garten", address: "Kastanienallee 7-9, 10435 Berlin, Germany", phone_number: "030", category: "belgian" },
+]
+restaurants_data.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
